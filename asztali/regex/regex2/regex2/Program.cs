80881f087e181fcd7219c138009c4952e37c10a3
gyarmati.gabor.22 <gyarmati.gabor.22@F2-19>
2026-01-27 11:25:35 +0100

    kartyak keszen

---
+++ asztali/regex/regex2/regex2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+namespace regex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.ReadAllLines("romeoesjulia.txt");
+            Regex nevek = new Regex("^[A-Z]+[,\t\n]");
+           var eredmeny= nevek.Matches("romeoesjulia.txt");
+            Console.WriteLine(eredmeny[0]);
+
+        }
+    }
+}
